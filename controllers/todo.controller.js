1255c16a5a09087ff6fab472fe4b0ba23e85940c
Md. Ariful Islam <ariful@pop-os.localdomain>
2022-08-06 01:20:59 +0600

    Bug fixed all the apis working fine

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -1,23 +1,22 @@
 const mongoose = require('mongoose')
 const Todo = require('../models/todo.model')
 // GET all todos
-const getAllTodos = (req, res) => {
+const getAllTodos = async (req, res) => {
     try {
-
-        res.status(200).json()
+        const result = await Todo.find()
+        res.status(200).json({ message: "Displayed all todos!", data: result })
     } catch (error) {
         res.status(500).json({ message: "Can't get all todos! - please try again later." })
-
     }
 }
 
 // GET a todo by ID
-const getSingleTodo = (req, res) => {
+const getSingleTodo = async (req, res) => {
     try {
-
-        res.status(200).json()
+        const result = await Todo.findById({ _id: req.params.id })
+        res.status(200).json({ message: "Found sucessfully!", data: result })
     } catch (error) {
-        res.status(500).json({ message: "Can't get all todos! - please try again later." })
+        res.status(500).json({ message: "Can't found the todo! - please try again later." })
 
     }
 }
@@ -26,9 +25,7 @@ const getSingleTodo = (req, res) => {
 const createOneTodo = async (req, res) => {
     try {
         const createTodo = new Todo(req.body)
-
         await createTodo.save()
-
         res.status(200).json({
             message: "Todo inserted sucessfully!"
         })
@@ -38,14 +35,12 @@ const createOneTodo = async (req, res) => {
 }
 
 // POST Multiples todos
-const createManyTodo = (req, res) => {
+const createManyTodo = async (req, res) => {
     try {
-
-
-        res.status(200).json()
+        const result = await Todo.insertMany(req.body)
+        res.status(200).json({ message: "Insertion of many is completed!", data: result })
     } catch (error) {
         res.status(500).json({ message: "Can't get all todos! - please try again later." })
-
     }
 }
 
@@ -71,12 +66,12 @@ const updateOneTodo = async (req, res) => {
 }
 
 // DELETE a todo
-const deleteOneTodo = (req, res) => {
+const deleteOneTodo = async (req, res) => {
     try {
-
-        res.status(200).json()
+        await Todo.findByIdAndDelete(req.params.id)
+        res.status(200).json({ message: "Todo deleted sucessfully!" })
     } catch (error) {
-        res.status(500).json({ message: "Can't get all todos! - please try again later." })
+        res.status(500).json({ message: "Can't delete todo! - please try again later." })
 
     }
 }
--- routes/todo.route.js
+++ routes/todo.route.js
@@ -3,9 +3,9 @@ const todoController = require('../controllers/todo.controller')
 
 router.get('/', todoController.getAllTodos)
 router.post('/', todoController.createOneTodo)
+router.post('/all', todoController.createManyTodo)
 router.get('/:id', todoController.getSingleTodo)
 router.put('/:id', todoController.updateOneTodo)
 router.delete('/:id', todoController.deleteOneTodo)
-router.get('/all', todoController.createManyTodo)
 
 module.exports = router;
