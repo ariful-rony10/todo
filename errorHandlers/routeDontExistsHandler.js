b1f789a575052563b6daab94aa2dc0bc52e80703
Md. Ariful Islam <hi@ariful.me>
2023-05-08 18:30:10 +0600

    Feat: Add Task

--- errorHandlers/routeDontExistsHandler.js
+++
@@ -1,4 +0,0 @@
-const routeDontExitsHander = (req, res, next) => {
-    res.status(404).send('Request url not found.')
-}
-module.exports = routeDontExitsHander
--- routes/user.route.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const authController = require('../controllers/auth.controller')
-
-// Signup
-
-router.post('/signup', authController.createUser)
-router.post('/login', authController.loginUser)
-
-module.exports = router;
